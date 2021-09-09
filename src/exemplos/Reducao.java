e9e815677171cb8c0a3f762d280fd988e34de67e
Luciano Ortiz <lucianoortizsilva@gmail.com>
2021-09-09 15:10:48 -0300

    finalizando reduction

--- src/exemplos/Reducao.java
+++ src/exemplos/Reducao.java
@@ -1,6 +1,13 @@
 package exemplos;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Reducao {
@@ -35,7 +42,7 @@ public class Reducao {
 		System.out.println("palavra: " + palavra);//
 
 		System.out.println();
-		
+
 		// REDUCE SEM PARALELO
 		List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);//
 		Integer n = numbers.stream().reduce(0, (a, b) -> a - b);// -21
@@ -58,6 +65,38 @@ public class Reducao {
 		String animal2 = animais2.parallelStream().reduce("X", String::concat);//
 		System.out.println("animal: " + animal2 + " com parallelStream();");//
 
+		System.out.println();
+
+		// PARALELO
+		Stream<String> musculos = Stream.of("perna", "biceps", "panturrilha", "abdomem").parallel();
+		SortedSet<String> set = musculos.collect(ConcurrentSkipListSet::new, Set::add, Set::addAll);
+		System.out.println(set);
+
+		System.out.println();
+
+		// PARALELO
+		Stream<String> bichos = Stream.of("leao", "tamandua", "gorila", "gato", "bem-te-vi").parallel();
+		ConcurrentMap<Integer, String> concurrentMap = bichos.collect(Collectors.toConcurrentMap(String::length, k -> k, (s1, s2) -> s1 + "," + s2));
+		System.out.println(concurrentMap);
+		System.out.println(concurrentMap.getClass());
+
+		System.out.println();
+
+		// PARALELO
+		Stream<String> bichos2 = Stream.of("leao", "tamandua", "gorila", "gato", "bem-te-vi").parallel();
+		ConcurrentMap<Integer, List<String>> concurrentMap2 = bichos2.collect(Collectors.groupingByConcurrent(String::length));
+		System.out.println(bichos.isParallel());
+		System.out.println(concurrentMap2);
+		System.out.println(concurrentMap2.size());
+		System.out.println(concurrentMap2.getClass());
+
+		//
+		List<Integer> sorteio = List.of(1,2,3,4,5,6,7,8,9,10);
+		var dados = Collections.synchronizedList(new ArrayList<Integer>());
+		sorteio.stream()//
+			.filter(s -> s % 2 == 0)//
+			.forEach(i -> dados.add(i));//
+		System.out.println("dados: " + dados);
 	}
 
 }
